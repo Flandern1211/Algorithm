7f920a58bc05ab0775275b973d3fc254e4048641
Flandern1211 <3180066912wzw@gmail.com>
2025-10-11 21:29:47 +0800

    feat(Leetcode): 添加删除排序链表中的重复元素函数
    - 实现了deleteDuplicates03函数，使用双指针法处理链表- 创建了两个虚拟头节点分别存储重复和非重复元素
    - 遍历原链表，根据元素是否重复将其分配到对应链表
    - 处理了最后一个节点的特殊情况- 返回非重复链表的头节点

--- Leetcode/83.go
+++ Leetcode/83.go
@@ -1,1 +1,30 @@
 package Leetcode
+
+// 1.链表的分解法
+func deleteDuplicates03(head *ListNode) *ListNode {
+	dummy01 := &ListNode{Val: 101}
+	dummy02 := &ListNode{Val: 101}
+
+	p1, p2 := dummy01, dummy02
+	p := head
+	for p != nil {
+		if p.Next != nil && p.Val == p.Next.Val {
+			p2.Next = p
+			p2 = p2.Next
+		}
+		if p.Next != nil && p.Val != p.Next.Val {
+			p1.Next = p
+			p1 = p1.Next
+		}
+		//注意把最后一个节点处理了
+		if p.Next == nil {
+			p1.Next = p
+			p1 = p1.Next
+		}
+		p = p.Next
+	}
+	p1.Next = nil
+	p2.Next = nil
+	//返回的应该是非重复链表的虚拟头结点的下一个
+	return dummy01.Next
+}
